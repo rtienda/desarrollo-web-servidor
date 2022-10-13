f698236dde01788ce9a11388daf4d5a2a808c1e7
rtienda <rtiendagonzalez@gmail.com>
2022-10-13 13:47:28 +0200

    ultimo commit del dia

---
+++ Desarrollo aplicaciones servidor/02formulario/ejercicios/ejercicio8.php
@@ -0,0 +1,10 @@
+<?php
+// EJERCICIO 8
+
+// Crear un formulario que reciba un número. Mostrar la tabla de multiplicar de ese número.
+// Hacerlo mediante una tabla HTML. 
+
+
+
+
+?>
--- Desarrollo aplicaciones servidor/02formulario/index2.php
+++ Desarrollo aplicaciones servidor/02formulario/index2.php
@@ -15,10 +15,10 @@
     <table class="customTable">
         <tr>
             <th>Enunciado</th>
-            <th style="min-width: 80px;">Enlace</th>
+            <!-- <th style="min-width: 80px;">Enlace</th> -->
         </tr>
         <tr>
-          <td><p id="ej1">Formulario get/post</p>
+          <td><p id="ej1"><b>Ejercicio 1 </b>Formulario get/post</p>
             <form action="#ej1" method="post">
                 <label>Nombre</label>
                 <input type="text" name="nombre"><br></br>
@@ -38,31 +38,31 @@
                 }
             ?>
           </td>
-          <td><a href="ejercicios/ejercicio1.php"><button>Ejercicio1</button></a>
+          <!-- <td><a href="ejercicios/ejercicio1.php"><button>Ejercicio1</button></a>
         
-        </td>
+        </td> -->
         </tr>
         <tr>
-            <td>Ejercicio de introducir un numero y pasarlo como parametro</td>
-            <td><a href="ejercicios/ejercicio2.php"><button>Ejercicio2</button></a></td>
+            <td><b>Ejercicio 2 </b>Ejercicio de introducir un numero y pasarlo como parametro</td>
+            <!-- <td><a href="ejercicios/ejercicio2.php"><button>Ejercicio2</button></a></td> -->
         </tr>
           <tr>
-            <td>Crear un formulario que reciba el nombre y la edad de una persona y
+            <td><b>Ejercicio 3 </b>Crear un formulario que reciba el nombre y la edad de una persona y
                 muestre por pantalla si es menor de edad, es adulta, o está jubilada en función
                 a su edad. Además:
                     - Convertir la edad a un número entero
                     - Convertir el nombre introducido a minúsculas salvo la primera letra, que será mayúscula</td>
-            <td><a href="ejercicios/ejercicio3.php"><button>Ejercicio3</button></a></td>
+            <!-- <td><a href="ejercicios/ejercicio3.php"><button>Ejercicio3</button></a></td> -->
         </tr>
         <tr>
-            <td>Crear un formulario que reciba una frase y un número del 1 al 6. Habrá que mostrar la frase
+            <td><b>Ejercicio 4 </b>Crear un formulario que reciba una frase y un número del 1 al 6. Habrá que mostrar la frase
                 en un encabezado de dicho número.
             
                 Si introducimos "5" y "Hola mundo" se mostrará un encabezado h5Hola mundoh5</td>
-            <td><a href="ejercicios/ejercicio4.php"><button>Ejercicio4</button></a></td>
+            <!-- <td><a href="ejercicios/ejercicio4.php"><button>Ejercicio4</button></a></td> -->
         </tr>
         <tr>
-            <td><p>Formulario que reciba dos números. Devolver el resultado de elevar el primero al segundo.</p> 
+            <td><p><b>Ejercicio 5 </b>Formulario que reciba dos números. Devolver el resultado de elevar el primero al segundo.</p> 
             <form action="" method="post" >
                 <label>Base</label>
                 <input type="number" name="base" class="numberButton"><br></br>
@@ -88,10 +88,10 @@
                 }
             ?>
             </td>
-            <td><a href="ejercicios/ejercicio5.php"><button>Ejercicio5</button></a></td>
+            <!-- <td><a href="ejercicios/ejercicio5.php"><button>Ejercicio5</button></a></td> -->
         </tr>
         <tr>
-            <td><p id="ej6">Formulario que reciba un número y devuelva su factorial.</p>
+            <td><p id="ej6"><b>Ejercicio 6 </b>Formulario que reciba un número y devuelva su factorial.</p>
             <form action="#ej6" method="post" >
                 <label>Factorial de:</label>
                 <input type="number" name="factorial" class="numberButton"><br></br>
@@ -115,8 +115,45 @@
                 }
             ?>
             </td>
-            <td><a href="ejercicios/ejercicio6.php"><button>Ejercicio6</button></a></td>
+            <!-- <td><a href="ejercicios/ejercicio6.php"><button>Ejercicio6</button></a></td> -->
         </tr>
+        <tr>
+        <td><p id="ej8"><b>Ejercicio 8 </b>Crear un formulario que reciba un número. Mostrar la tabla de multiplicar de ese número.</p>
+            <form action="#ej8" method="post" >
+                <label>Tabla de multiplicar de:</label>
+                <input type="number" name="numero" class="numberButton"><br></br>
+                <input type="hidden" name="f" value="ej8">
+                <input type="submit" value="Enviar" class="button1">
+
+            </form>
+            <?php
+                if($_SERVER["REQUEST_METHOD"]=="POST"){
+                    if($_POST["f"]=="ej8"){
+                        $numero = $_POST["numero"];
+
+                        
+
+                        //$resultado = factorial($factorial);
+                        if($numero == -1){
+                            echo "<p>El numero no puede ser negativo o cero</p>";
+
+                        }else{
+                            echo "<table class='customTable'>";
+                            echo "<tr><th>Operacion</th><th>Resultado</th></tr>";
+                            for($i=1;$i<=10;$i++){
+                                $resultado=$numero*$i;
+                                echo "<tr><td>$numero x $i</td><td>$resultado</td></tr>";
+                            }
+                            echo "</table>";
+                        
+                        }
+                    }
+                }
+            ?>
+            </td>
+
+        <tr>
+
       </table>
     </div>
 </body>
