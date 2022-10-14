13c88fe17d9a122a4693118e8fa7af1cebf95b18
rtienda <rtiendagonzalez@gmail.com>
2022-10-14 09:44:41 +0200

    Formulario validaciones

---
+++ Desarrollo aplicaciones servidor/03variacionFormulario/videojuegos/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <h2>Nuevo videojuego</h2>
+    <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            // echo htmlspecialchars($_POST["titulo"]);
+            // echo "<br>";
+            // echo htmlspecialchars($_POST["precio"]);
+            // var_dump(trim($_POST["titulo"]));
+            // var_dump(stripslashes($_POST["titulo"]));
+
+            // echo depurar($_POST["titulo"]);
+            // echo "<br>";
+            // echo depurar($_POST["precio"]);
+
+            $temp_titulo = depurar($_POST["titulo"]);
+            $temp_precio = depurar($_POST["precio"]);
+            if(empty($temp_titulo)){
+                $err_titulo = "El titulo es obligatorio";
+            }
+        }
+        function depurar($dato){
+            $dato = trim($dato);
+            $dato = stripslashes($dato);
+            $dato=htmlspecialchars($dato);
+            return $dato;
+        }
+            
+    ?>
+    <form action="" method="post">
+        <p>Titulo: <input type="text" name="titulo">
+        <span class="error">
+            * <?php if(isset($err_titulo)) echo $err_titulo ?>
+        </span></p>
+        <p>Precio: <input type="text" name="precio"></p>
+        <p><input type="submit" name="Crear"></p>
+    </form>
+
+    
+</body>
+</html>
