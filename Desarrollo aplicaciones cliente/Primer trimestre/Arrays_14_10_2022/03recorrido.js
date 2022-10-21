cb50579a49e6b60ee955ef35088fc84540f0dd57
rtienda <rtiendagonzalez@gmail.com>
2022-10-21 14:10:53 +0200

    Ejercicios1

--- Desarrollo aplicaciones cliente/Primer trimestre/Arrays_14_10_2022/03recorrido.js
+++ Desarrollo aplicaciones cliente/Primer trimestre/Arrays_14_10_2022/03recorrido.js
@@ -14,3 +14,9 @@ arreglo.forEach(palabra =>  {
 });
 }
 mostrar(parrafo);
+
+var elem = prompt("del parrafo **"+parrafo+"**, que elemento quiere borrar");
+var cant = prompt("del parrafo **"+parrafo+"**, cuantos elementos quiere borrar");
+
+parrafo.splice(elem,cant);
+alert("resultado: "+parrafo);
