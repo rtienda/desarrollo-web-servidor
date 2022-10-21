eafe7c54c0ae074b315a7b3fc24d583c9b0bcc17
rtienda <rtiendagonzalez@gmail.com>
2022-10-21 10:36:51 +0200

    Ejercicios1

--- practica_01/funciones/dniValido.php
+++ practica_01/funciones/dniValido.php
@@ -2,34 +2,67 @@
 
     function dniValido($dni){
         $resultado=false;
-        if(primerosOcho($dni)){
+        if(strlen($dni)==9){
+            if(primerosOcho($dni)){
            
-            if(letraValida($dni)){
-                echo "bien";
-                $resultado=true;
+                if(letraValida($dni)){
+                    //echo "bien";
+                    $resultado=true;
+                }
             }
         }
+       
         return $resultado;
     }
 
     function primerosOcho($dni){
         $result=true;
         for($i=1;$i<strlen($dni)-1;$i++){
-            if(is_numeric(substr($dni,$i,1))){
-                
-            }else{
-            
-
+            if(!is_numeric(substr($dni,$i,1))){
                 $result=false;
             }
-            
-        }
         return $result;
+        }
     }
+
     function letraValida($dni){
         $letra=substr($dni,strlen($dni)-1,1);
-        
-        return true;
+        $numero=substr($dni,0,8);
+        //echo "letra: $letra</br>";
+        //echo "nunero: $numero</br>";
+        $calculo=$numero%23;
+        //echo "calculo: $calculo</br>";
+        $letra_correcta=match ($calculo){
+            0 => "T",
+            1 => "R",
+            2 => "W",
+            3 => "A",
+            4 => "G",
+            5 => "M",
+            6 => "Y",
+            7 => "F",
+            8 => "P",
+            9 => "D",
+            10 => "X",
+            11 => "B",
+            12 => "N",
+            13 => "J",
+            14 => "Z",
+            15 => "S",
+            16 => "Q",
+            17 => "V",
+            18 => "H",
+            19 => "L",
+            20 => "C",
+            21 => "K",
+            22 => "E",
+            default => "PP",
+        };
+        if($letra_correcta==strtoupper($letra)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
--- practica_01/index.php
+++ practica_01/index.php
@@ -15,12 +15,12 @@
         <input type="number" name="primerNumero" class="numberButton"><br></br>
         <label>Segundo numero</label>
         <input type="number" name="segundoNumero" class="numberButton"><br></br>
-        <intput type="hidden" name="f" value="ej1">
+        <input type="hidden" name="f" value="ej1">
         <input type="submit" value="Enviar" class="button1">
     </form>
     <?php
         if($_SERVER["REQUEST_METHOD"]=="POST"){
-            if($_POST["f"]="ej1"){
+            if($_POST["f"]=="ej1"){
                 require 'funciones/EsPrimo.php';
                 $longitud = $_POST["primerNumero"];
                 $inicial = $_POST["segundoNumero"];
@@ -45,12 +45,12 @@
     <form action="#ej2" method="post">
         <label>Inserte DNI</label>
         <input type="text" name="dni"><br></br>
-        <intput type="hidden" name="f" value="ej2">
+        <input type="hidden" name="f" value="ej2">
         <input type="submit" value="Enviar" class="button1">
     </form>
     <?php
         if($_SERVER["REQUEST_METHOD"]=="POST"){
-            if($_POST["f"]="ej2"){
+            if($_POST["f"]=="ej2"){
                 require "funciones/dniValido.php";
                 $dni=$_POST["dni"];
                 if(dniValido($dni)){
@@ -58,6 +58,22 @@
                 }else{
                     echo "<p>El Dni no es valido</p>";
                 }
+            }
+        }
+    ?>
+
+    <div>-----------------------------</div>
+    <p id="ej2">Ejercicio 3</p>
+        <form action="#ej2" method="post">
+            
+            <input type="hidden" name="f" value="ej3">
+            <input type="submit" value="Enviar" class="button1" style="margin: bottom 20px;">
+        </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            if($_POST["f"]=="ej3"){
+                require "funciones/ConstruyeTablas.php";
+                multiplicarNumero();
             }
         }
     ?>
